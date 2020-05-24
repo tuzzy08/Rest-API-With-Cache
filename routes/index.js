4baa40467a758d36cf349bb3e1922a3d3bbe11d2
Sage <tuzzy08@gmail.com>
2020-05-25 00:34:27 +0100

    Revert "Develop"

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const postController = require('../controllers/postController');
-const validator = require('../utils/validators');
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', (request, response, next) => {
-  res.render('index', {
-    title: 'Express'
-  });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-
-router.get('/posts/:postId', postController.getPost);
-router.get('/posts', postController.getAllPosts);
-router.post('/posts/edit/:postId', postController.editPost);
-router.post('/posts/create', postController.createPost);
-router.get('/posts/delete/:postId', postController.deletePost);
 
 module.exports = router;
